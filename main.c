24cd26482b79bdbaa14ca26a0df86411bb3210e8
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2012-10-10 15:02:09 +0200

    made main.c c better (using color functions for servers/apps)

--- main.c
+++ main.c
@@ -20,6 +20,8 @@
 #include "ssmp.h"
 #include "ssmp_recv.h"
 #include "ssmp_send.h"
+#include "measurements.h"
+
 
 #define COLOR_BUF
 #define USE_MEMCPY
@@ -27,8 +29,36 @@
 //#define BLOCKING
 
 int num_procs = 2;
-long long int nm = 100000000;
+long long int nm = 1000000;
 int ID;
+uint32_t num_dsl = 0;
+uint32_t num_app = 0;
+uint32_t dsl_seq[48];
+
+uint32_t wcycles = 2;
+
+int color_dsl(int id)
+{
+  return (id % wcycles == 0);
+}
+
+uint32_t nth_dsl(int id)
+{
+  int i, id_seq = 0;
+  for (i = 0; i < ssmp_num_ues(); i++)
+    {
+      if (i == id)
+	{
+	  return id_seq;
+	}
+      id_seq++;
+    }
+}
+
+int color_app1(int id)
+{
+  return !(color_dsl(id));
+}
 
 int main(int argc, char **argv) {
   if (argc > 1) {
@@ -38,11 +68,38 @@ int main(int argc, char **argv) {
     nm = atol(argv[2]);
   }
 
+  if (argc > 3)
+    {
+      wcycles = atoi(argv[3]);
+    }
+
   ID = 0;
   printf("NUM of processes: %d\n", num_procs);
   printf("NUM of msgs: %lld\n", nm);
 
+
+  uint32_t i, dsl_seq_idx = 0;;
+  for (i = 0; i < num_procs; i++)
+    {
+      if (color_dsl(i))
+	{
+	  printf("%2d -> DSL\n", i);
+	  num_dsl++;
+	  dsl_seq[dsl_seq_idx++] = i;
+	}
+      else
+	{
+	  printf("%2d -> APP\n", i);
+	  num_app++;
+	}
+    }
+
+  PRINT("dsl: %2d | app: %d", num_dsl, num_app);
+
   ssmp_init(num_procs);
+
+  ssmp_barrier_init(2, 0, color_dsl);
+  ssmp_barrier_init(1, 0, color_app1);
 
   int rank;
   for (rank = 1; rank < num_procs; rank++) {
@@ -62,10 +119,10 @@ int main(int argc, char **argv) {
   ssmp_mem_init(ID, num_procs);
 
   ssmp_color_buf_t *cbuf = NULL;
-  if (ID % 2 == 0) {
+  if (color_dsl(ID)) {
     cbuf = (ssmp_color_buf_t *) malloc(sizeof(ssmp_color_buf_t));
     assert(cbuf != NULL);
-    ssmp_color_buf_init(cbuf, color_app);
+    ssmp_color_buf_init(cbuf, color_app1);
   }
 
   volatile ssmp_msg_t *msg;
@@ -74,37 +131,71 @@ int main(int argc, char **argv) {
 
   uint32_t last_recv_from = 0;
 
+  PF_MSG(0, "recv");
+  PF_MSG(1, "send");
+  PF_MSG(2, "roundtrip");
+
+
+  uint32_t num_zeros = num_app;
+  uint32_t lim_zeros = num_dsl;
   ssmp_barrier_wait(0);
-  P("CLEARED barrier %d", 0);
 
+  if (color_dsl(ID)) {
+    PRINT("dsl core");
 
-  double _start = wtime();
-  ticks _start_ticks = getticks();
-
-  if (ID % 2 == 0) {
     while(1) {
-      //ssmp_recv_color(cbuf, msg, 24);
-      last_recv_from = ssmp_recv_color_start(cbuf, msg, last_recv_from + 1);
+      //      wait_cycles(wcycles);
 
-      if (msg->w0 < 0) {
-	P("exiting ..");
-	exit(0);
+      PF_START(0);
+      last_recv_from = ssmp_recv_color_start(cbuf, msg, last_recv_from + 1);
+      PF_STOP(0);
+
+      //      PRINT(" <<<<<<<<<<<<< %4d from %d", msg->w0, msg->sender);
+
+      PF_START(1);
+      ssmp_send(msg->sender, msg, 64);
+      PF_STOP(1);
+
+      if (msg->w0 < lim_zeros) {
+	if (--num_zeros == 0)
+	  {
+	    P("done ..");
+	    break;
+	  }
       }
-      ssmp_send(msg->sender, msg, 8);
-      //      ssmp_sendm(msg->sender, msg);
-      //ssmp_send_inline(msg->sender, msg);
+
     }
   }
   else {
-    unsigned int to = ID-1;
+    //    unsigned int to = ID-1;
+    unsigned int to = 0, to_idx = 0;
     long long int nm1 = nm;
+
+    ssmp_barrier_wait(1);
     while (nm1--) {
-      to = (to + 2) % num_procs;
-      //to = 0;
+      PF_START(2);
+      to = dsl_seq[to_idx++];
+      if (to_idx == num_dsl)
+	{
+	  to_idx = 0;
+	}
+      PREFETCHW(ssmp_send_buf[dsl_seq[to_idx]]);
+      //      PRINT("%6d >> %d", nm1, to);
+
 
       msg->w0 = nm1;
-      ssmp_send(to, msg, 24);
-      ssmp_recv_from(to, msg, 24);
+      PF_START(1);
+      ssmp_send(to, msg, 64);
+      PF_STOP(1);
+
+      //      wait_cycles(wcycles);
+
+      PF_START(0);
+      ssmp_recv_from(to, msg, 64);
+      PF_STOP(0);
+      PF_STOP(2);
+
+      //      PRINT("%6d << %d", msg->w0, to);
 
       if (msg->w0 != nm1) {
 	P("Ping-pong failed: sent %lld, recved %d", nm1, msg->w0);
@@ -113,46 +204,17 @@ int main(int argc, char **argv) {
   }
 
   
-
-  ticks _end_ticks = getticks();
-  double _end = wtime();
-
-  double _time = _end - _start;
-  ticks _ticks = _end_ticks - _start_ticks;
-  ticks _ticksm =(ticks) ((double)_ticks / nm);
-  double lat = (double) (1000*1000*1000*_time) / nm;
-
-
   uint32_t c;
   for (c = 0; c < ssmp_num_ues(); c++)
     {
       if (c == ssmp_id())
-	{
-	  printf("[%02d] sent %lld msgs\n\t"
-		 "in %f secs\n\t"
-		 "%.2f msgs/us\n\t"
-		 "%f ns latency\n"
-		 "in ticks:\n\t"
-		 "in %lld ticks\n\t"
-		 "%lld ticks/msg\n", 
-		 c, nm, _time, ((double)nm/(1000*1000*_time)), lat,
-		 _ticks, _ticksm);
-	}
-      ssmp_barrier_wait(1);
+  	{
+	  PF_PRINT;
+  	}
+      ssmp_barrier_wait(0);
     }
 
-  ssmp_barrier_wait(1);
-  if (ssmp_id() == 1) {
-    P("terminating --");
-    int core; 
-    for (core = 0; core < ssmp_num_ues(); core++) {
-      if (core % 2 == 0) {
-	ssmp_msg_t s; s.w0 = -1;
-	ssmp_send(core, &s, 24);
-      }
-    }
-  }
-
+  ssmp_barrier_wait(3);
   ssmp_term();
   return 0;
 }
