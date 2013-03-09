b6f928592f2b72d0da6d69ef25088a13b8346c32
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2013-03-09 23:07:02 +0100

    fix: prefetw on recv

--- ssmp_recv.c
+++ ssmp_recv.c
@@ -172,8 +172,8 @@ ssmp_recv_color_start(ssmp_color_buf_t *cbuf, ssmp_msg_t *msg)
 		{
 		  start_recv_from = 0;
 		}
-	      PREFETCHW(ssmp_send_buf[msg->sender]);
-	      PREFETCHW(buf[start_recv_from]);
+	      /* PREFETCHW(ssmp_send_buf[msg->sender]); */
+	      /* PREFETCHW(buf[start_recv_from]); */
 
 	      return;
 	    }
