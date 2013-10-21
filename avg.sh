bd38bf40a9f421f604ce163474638d30e9b9d185
David Cervini <cervini@lpd48core.epfl.ch>
2013-10-21 10:31:38 +0200

    script to compute avg

---
+++ avg.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+for command in "./threadone2one" "./one2one"
+do
+	echo $command
+	for pair in 2 4 8 16 32 48 64 96
+	do
+		awk -v e="$command $pair" ' BEGIN{sum = 0; count = 0} $0 ~ e {sum += $3; count++} END{print e, count, (sum/count)} ' measures 
+	done
+done
+
