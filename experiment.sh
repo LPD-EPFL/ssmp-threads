f4de5461fda76e35d524341690f91c7918293731
David Cervini <cervini@lpd48core.epfl.ch>
2013-10-21 09:53:03 +0200

    script for taking the throughput measures

---
+++ experiment.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+for command in "./threadone2one" "./one2one"
+do
+	echo $command
+	for pair in 2 4 8 16 32 48 64 96
+	do
+		for i in 1 2 3
+		do
+			echo "----------"
+			echo $pair
+			$command $pair > tmp
+			awk -v p=$pair -v c=$command '/Throughput/ {print c,p,$4}' tmp >> measures	
+		done
+	done
+done
