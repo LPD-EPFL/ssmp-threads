5f75b62936d09fa5ae4d2d2b90b2c3c5468c6c1d
David Cervini <cervini@lpd48core.epfl.ch>
2013-10-23 16:16:12 +0200

    new version of experiment.sh

--- experiment.sh
+++ experiment.sh
@@ -1,16 +1,30 @@
-#! /bin/sh
+#!/bin/bash
 
 for command in "./threadone2one" "./one2one"
 do
+	x=0 #x thread on core 0 on die 0
+	declare -A location
+	location[1]="same-die" #both on die 0
+	location[6]="same-mcm" #die 1
+	location[24]="one-hop" #die 4
+	location[12]="two-hop" #die 2, ie longest path 
+	echo "----------"
 	echo $command
-	for pair in 2 4 8 16 32 48 64 96
+	for core in ${!location[@]}
 	do
+		lo=${location[$core]}
+		echo $lo
 		for i in 1 2 3
 		do
-			echo "----------"
-			echo $pair
-			$command $pair > tmp
-			awk -v p=$pair -v c=$command '/Throughput/ {print c,p,$4}' tmp >> measures	
+			$command -x $x -y $core  > tmp
+			if [ $? -eq 0 ] 
+			then
+				awk -v c=$command -v dist=$lo \
+					'BEGIN {ticks=0} 
+					/ticks/ {ticks=$18} 
+					/Throughput/ {print c, dist, ticks, $4; ticks=-1}'\
+					tmp >> measures	
+			fi
 		done
 	done
 done
