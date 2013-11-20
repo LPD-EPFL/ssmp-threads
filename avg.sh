66148b6c5bcebb9ac8057988ee5eb4e65bf642e2
cervini <cervini@cervini.(none)>
2013-11-21 00:05:12 +0100

    first graph : oneway

--- avg.sh
+++ avg.sh
@@ -5,7 +5,14 @@ do
 	echo $command
 	for core in 6 12 18 24 30 36 48 
 	do
-		awk -v e="$command $core" 'BEGIN{sum = 0; count = 0} $0 ~ e {sum += $4; count++} END{print e, (sum/count/1000000)}' $1 
+		for dsl in $((core/1)) $((core/2)) $((core/3)) $((core/4)) $((core/5)) $((core/6))
+		do
+			awk -v e="$command $core $dsl" 'BEGIN{sum = 0; count = 0} 
+				$0 ~ e {sum += $5; count++} 
+				END{if (count>0) {
+				  	print e, (sum/count/1000000)
+				}}' $1 
+		done
 	done
 done
 
