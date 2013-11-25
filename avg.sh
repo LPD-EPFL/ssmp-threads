fd03ed94cd3326f1ca9c6b8fcdce56662cd42b34
David Cervini <cervini@lpd48core.epfl.ch>
2013-11-25 09:50:00 +0100

    avg.sh

--- avg.sh
+++ avg.sh
@@ -5,7 +5,7 @@ do
 	echo $command
 	for core in 6 12 18 24 30 36 48 
 	do
-		for dsl in $((core/1)) $((core/2)) $((core/3)) $((core/4)) $((core/5)) $((core/6))
+		for dsl in 2 3 4 5 6 
 		do
 			awk -v e="$command $core $dsl" 'BEGIN{sum = 0; count = 0} 
 				$0 ~ e {sum += $5; count++} 
